4b8cbec2479d123b3a0190ff872faa7fc9142a36
Vladimir Vukicevic <vladimir@pobox.com>
2008-03-25 17:37:19 -0700

    Add MPL license boilerplate to where it's missing

--- js/graph.js
+++ js/graph.js
@@ -1,3 +1,27 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is perf graph code.
+ *
+ * The Initial Developer of the Original Code is
+ *   Mozilla Corporation
+ * Portions created by the Initial Developer are Copyright (C) 2008
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Vladimir Vukicevic <vladimir@pobox.com>
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 // Just the single value for each result; reports value over time
 var GRAPH_TYPE_VALUE = 0;
