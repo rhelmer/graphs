901809232c1415044fab06f88511236d672c7c27
K Lars Lohn <lars@mozilla.com>
2009-01-02 09:54:44 -0800

    Bug 456529 - rewrote collect.cgi to reflect new schema added pyfomatic python package to centralize python routines
    --HG--
    branch : 1.0

---
+++ server/pyfomatic/collect.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python
+
+import sys
+import time
+import re
+
+
+#=================================================================================================================
+class Error(Exception):
+  pass
+#=================================================================================================================
+class ImproperFormatException(Error):
+  pass
+#=================================================================================================================
+class ValueException(Error):
+  pass
+#=================================================================================================================
+class DatabaseException(Error):
+  pass
+
+#=================================================================================================================
+class StringValidator(object):
+  reString = re.compile('^[0-9A-Za-z._()\- ]*$')
+  @staticmethod
+  def validate(aString):
+    """"""
+    if aString is None:
+      return ''
+    if StringValidator.reString.match(aString):
+      return aString
+    raise ImproperFormatException("'%s' failed string validation" % aString[:100])
+
+#=================================================================================================================
+class MetaDataFromTalos(object):
+  fieldNames = ["machine_name",           "test_name",              "branch_name",            "ref_changeset",          "ref_build_id","date_run"]
+  fieldTypes = [StringValidator.validate, StringValidator.validate, StringValidator.validate, StringValidator.validate, int,           int       ]
+  nameTypeAssociations = dict(zip(fieldNames, fieldTypes))
+  #-----------------------------------------------------------------------------------------------------------------
+  def __init__(self, databaseCursor, databaseModule, dataSource):
+    self.databaseCursor = databaseCursor
+    self.databaseModule = databaseModule
+    self.readFromSource(dataSource)
+    self.doDatabaseThings(databaseCursor)
+  #-----------------------------------------------------------------------------------------------------------------
+  def readFromSource (self, dataSource):
+    try:
+      dataSource = dataSource.readline()
+    except AttributeError:
+      pass
+    try:
+      dataSource = [x.strip() for x in dataSource.split(",")]
+    except AttributeError:
+      pass
+    numberOfInputFields = len(dataSource)
+    numberOfRequiredFields = len(MetaDataFromTalos.fieldNames)
+    if numberOfInputFields != numberOfRequiredFields:
+      raise ImproperFormatException("Bad format for metadata - %d instead of %d fields" % (numberOfInputFields, numberOfRequiredFields))
+    for name, value in zip(MetaDataFromTalos.fieldNames, dataSource):
+      try:
+        convertedValue = MetaDataFromTalos.nameTypeAssociations[name](value)
+        setattr(self, name, convertedValue)
+      except Exception, x:
+        raise ImproperFormatException(str(x))
+  #-----------------------------------------------------------------------------------------------------------------
+  def doDatabaseThings (self, databaseCursor):
+    # get machine_id
+    try:
+      databaseCursor.execute("select id from machines where name = %s", (self.machine_name,))
+      self.machine_id = databaseCursor.fetchall()[0][0]
+    except (self.databaseModule.Error, IndexError), x:
+      databaseCursor.connection.rollback()
+      raise DatabaseException("No machine_name called '%s' can be found" % self.machine_name)
+    # get os_id
+    #try:
+      #databaseCursor.execute("select os_id from machines where id = %s", (self.machine_id,))
+      #self.os_id = databaseCursor.fetchall()[0][0]
+    #except (self.databaseModule.Error, IndexError), x:
+      #databaseCursor.connection.rollback()
+      #raise DatabaseException("No os_id for a machine_id '%s' can be found" % self.machine_id)
+    # get test_id
+    try:
+      databaseCursor.execute("select id from tests where name = %s", (self.test_name,))
+      self.test_id = databaseCursor.fetchall()[0][0]
+    except (self.databaseModule.Error, IndexError), x:
+      databaseCursor.connection.rollback()
+      raise DatabaseException("No test_name called '%s' can be found" % self.test_name)
+    # get branch_id
+    try:
+      databaseCursor.execute("select id from branches where name = %s", (self.branch_name,))
+      self.branch_id = databaseCursor.fetchall()[0][0]
+    except (self.databaseModule.Error, IndexError), x:
+      databaseCursor.connection.rollback()
+      raise DatabaseException("No branch_id for a branch_name '%s' can be found" % self.branch_name)
+    # get build_id
+    try:
+      databaseCursor.execute("select id from builds where branch_id = %s and ref_build_id = %s and ref_changeset = %s",
+                             (self.branch_id, self.ref_build_id, self.ref_changeset))
+      self.build_id = databaseCursor.fetchall()[0][0]
+    except (self.databaseModule.Error, IndexError), x:
+      try:
+        databaseCursor.connection.rollback()
+        databaseCursor.execute("""insert into builds
+                                  (ref_build_id,      ref_changeset,      branch_id,      date_added) values
+                                  (%s,                %s,                 %s,             %s)""",
+                                  (self.ref_build_id, self.ref_changeset, self.branch_id, int(time.time())))
+        databaseCursor.execute("select id from builds where branch_id = %s and ref_build_id = %s and ref_changeset = %s",
+                               (self.branch_id, self.ref_build_id, self.ref_changeset))
+        self.build_id = databaseCursor.fetchall()[0][0]
+      except (self.databaseModule.Error, IndexError), x:
+        databaseCursor.connection.rollback()
+        raise DatabaseException("Unable to create a build with unique keys: branch_id:'%s', ref_build_id:'%s', ref_changeset:'%s'" % (self.branch_id, self.ref_build_id, self.ref_changeset))
+
+    #create new test_run record
+    try:
+      databaseCursor.execute("""select max(run_number)
+                                from test_runs
+                                where machine_id = %s and test_id = %s and branch_id = %s""",
+                                (self.machine_id, self.test_id, self.branch_id))
+      self.run_number = databaseCursor.fetchall()[0][0] + 1
+    except (self.databaseModule.Error, IndexError), x:
+      databaseCursor.connection.rollback()
+      self.run_number = 0
+    try:
+      databaseCursor.execute("""insert into test_runs
+                                (machine_id,      test_id,      build_id,      run_number,      date_run) values
+                                (%s,              %s,           %s,            %s,              %s)""",
+                                (self.machine_id, self.test_id, self.build_id, self.run_number, self.date_run))
+      databaseCursor.execute("""select id from test_runs
+                                where machine_id = %s and test_id = %s and build_id = %s and run_number = %s""",
+                                (self.machine_id, self.test_id, self.build_id, self.run_number))
+      self.test_run_id = databaseCursor.fetchall()[0][0]
+    except (self.databaseModule.Error, IndexError), x:
+      databaseCursor.connection.rollback()
+      raise DatabaseException("unable to insert new record into 'test_runs': %s" % str(x))
+    databaseCursor.connection.commit()
+
+
+#=================================================================================================================
+
+#-----------------------------------------------------------------------------------------------------------------
+def _updateAverageForTestRun(average, databaseCursor, inputStream, metadata):
+  try:
+    databaseCursor.execute("""update test_run set average = %s where id = %s""", (average, metadata.test_run_id))
+  except Exception, x:
+    databaseCursor.connection.rollback()
+    raise DatabaseException("unable to update average 'test_run' for id:%s : %s" % (metadata.test_run_id, str(x)))
+
+#-----------------------------------------------------------------------------------------------------------------
+def valuesReader(databaseCursor, databaseModule, inputStream, metadata):
+  sum = 0
+  for lineNumber, aLine in enumerate(inputStream):
+    if aLine.upper() in 'END':
+      break
+    values = [x.strip() for x in aLine.split(',')]
+    numberOfValues = len(values)
+    if numberOfValues == 2:
+      values.append('NULL')
+    elif numberOfValues != 3:
+      raise ImproperFormatException("value set #%d does not have the correct number of values: '%s'" % (lineNumber, aLine))
+    try:
+      values[0] = int(values[0])
+      values[1] = float(values[1])
+      sum += values[1]
+      try:
+        if values[2].lower() == 'null':
+          page_id = None
+        else:
+          try:
+            databaseCursor.execute("select page_id from pages where page_name = %s", (values[2],))
+            page_id = databaseCursor.fetchall()[0][0]
+          except  (databaseModule.Error, IndexError), x:
+            databaseCursor.connection.rollback()
+            raise DatabaseException("no page_id found for page '%s': %s" % (values[2], str(x)))
+      except IndexError:
+        page_id = None
+    except ValueError, x:
+      raise ImproperFormatException("value set #%s has a bad value: %s" % (lineNumber, str(x)))
+    try:
+      databaseCursor.execute("""insert into test_run_values
+                                (test_run_id,          interval_id, value,     page_id) values
+                                (%s,                  %s,          %s,        %s""",
+                                (metadata.test_run_id, values[0],   values[1], page_id))
+    except Exception, x:
+      databaseCursor.connection.rollback()
+      raise DatabaseException("unable to insert new record into 'test_run_values': %s" % str(x))
+  try:
+    average = sum / lineNumber
+  except ZeroDivisionError:
+    raise ValueException("No values were found in this dataset")
+  _updateAverageForTestRun(average, databaseCursor, inputStream, metadata)
+  databaseCursor.connection.commit()
+  return average
+
+#-----------------------------------------------------------------------------------------------------------------
+def averageReader(databaseCursor, databaseModule, inputStream, metadata):
+  averageAsString = inputStream.readline()
+  try:
+    average = float(averageAsString)
+  except Exception, x:
+    raise ImproperFormatException("the read average was not a floating point number: '%s'" % averageAsString)
+  _updateAverageForTestRun(average, databaseCursor, inputStream, metadata)
+  databaseCursor.connection.commit()
+  return average
+
+#-----------------------------------------------------------------------------------------------------------------
+def handleRequest(theForm, databaseConnection, databaseModule=None, outputStream=sys.stdout):
+  if not databaseModule:
+    databaseModule = sys.modules[databaseConnection.__module__.split('.')[0]]
+
+  exitCode = None
+  responseList = ["Content-type: text/plain\n"]
+
+  try:
+    if "filename" not in theForm:
+      raise ImproperFormatException("Cannot find input stream")
+
+    inputStream = theForm["filename"].file
+    startLine = inputStream.readline()
+    if startLine.upper() not in 'START':
+      raise ImproperFormatException("input stream did not begin with 'START'")
+    dataSetType = inputStream.readline().upper()
+    if dataSetType not in ('VALUES', 'AVERAGE'):
+      raise ImproperFormatException("data set type was not 'VALUES' or 'AVERAGE'")
+
+    databaseCursor = databaseConnection.cursor()
+
+    metadata = MetaDataFromTalos(databaseCursor, databaseModule, inputStream)
+    if dataSetType == 'VALUES':
+      average = valuesReader(databaseCursor, databaseModule, inputStream, metadata)
+      responseList.append("RETURN:%s:graph.html#runid=%d" % (metadata.test_name, metadata.test_run_id))
+    else:
+      average = averageReader(databaseCursor, databaseModule, inputStream, metadata)
+    responseList.append("RETURN:%s:%.2f:graph.html#id=%d&branchid=%d&machineid=%d" % (metadata.test_name, average, metadata.test_id, metadata.branch_id, metadata.machine_id))
+
+  except Exception, x:
+    responseList.append(x)
+    exitCode = 500
+
+  for aResponseLine in responseList:
+    print >>outputStream, aResponseLine
+
+  return exitCode
---
+++ server/pyfomatic/tests/test_collect.py
@@ -0,0 +1,280 @@
+import py.test
+
+import re
+import StringIO
+
+import pyfomatic.collect as c
+
+#=================================================================================================================
+class FakeInputStream(object):
+  def __init__(self, dataSource):
+    self.dataSource = dataSource
+    self.currentIter = None
+  def readline(self):
+    if not self.currentIter:
+      self.currentIter = self._iter()
+    return self.currentIter.next()
+  def __iter__(self):
+    if not self.currentIter:
+      for x in self._iter():
+        yield x
+    else:
+      for x in self.currentIter:
+        yield x
+  def _iter(self):
+    for x in self.dataSource:
+      yield x
+
+#=================================================================================================================
+class FakeDatabaseModule(object):
+  Error = Exception
+
+#=================================================================================================================
+class FakeCursor(object):
+  spaceKillerRe = re.compile(r'\s+')
+  def __init__(self, selectLookup):
+    self.selectLookup = selectLookup
+    self.inTransaction = True
+    self.currentSelect = None
+    self.inserts = {}
+    self.connection = self
+  def execute(self, sql, parameters):
+    self.inTransaction = True
+    if "select" in sql:
+      self.currentSelect = parameters
+    elif "insert" in sql:
+      insertComponents = sql.split(' ')
+      self.inserts.setdefault(insertComponents[2].strip(), []).append(parameters)
+      self.recentInsert = parameters
+    return None
+  def commit(self):
+    self.inTransaction = False
+  rollback = commit
+  def fetchall(self):
+    try:
+      return [ (self.selectLookup[self.currentSelect],) ]
+    except KeyError:
+      return [ () ]
+  def cursor(self):
+    return self
+
+#=================================================================================================================
+class FakeForm(dict):
+  def __init__(self, dataSource):
+    class A(object):
+      pass
+    self["filename"] = file = A()
+    setattr(file, "file", FakeInputStream(dataSource))
+
+#=================================================================================================================
+metadataTest1 = [ "machine_1", "test_1", "branch_1", "changeset_1", 13, 1229477017]
+
+#-----------------------------------------------------------------------------------------------------------------
+databaseSelectResponsesTest1 = {
+  ("machine_1",): 234,
+  (234,): 1,  #os_id
+  ("test_1",): 45,
+  ("branch_1",): 3455,
+  (3455, 13,"changeset_1"): 2220,
+  (234, 45, 3455): 99,
+  (234, 45, 2220, 100): 6667,
+  ("page_01",): 1001,
+  ("page_02",): 1002,
+  ("page_03",): 1003,
+  ("page_04",): 1004,
+  ("page_05",): 1005,
+  ("page_06",): 1006,
+  ("page_07",): 1007,
+  ("page_08",): 1008,
+  ("page_09",): 1009,
+  ("page_10",): 1010,
+  ("page_11",): 1011,
+  ("page_12",): 1012,
+}
+
+#-----------------------------------------------------------------------------------------------------------------
+databaseSelectResponsesTest2 = {
+  ("machine_1",): 234,
+  (234,): 1,  #os_id
+  ("test_1",): 45,
+  #("branch_1",): 3455,
+  (3455, 13,"changeset_1"): 2220,
+  (234, 45, 3455): 99,
+  (234, 45, 2220, 100): 6667,
+  }
+
+#-----------------------------------------------------------------------------------------------------------------
+valuesList1 = [ "1, 1.0,page_01",
+                "2, 2.0,page_02",
+                "3, 3.0,page_03",
+                "4, 1.0,page_04",
+                "5, 2.0,page_05",
+                "6, 3.0,page_06",
+                "7, 1.0,page_07",
+                "8, 2.0,page_08",
+                "9, 3.0,page_09",
+                "10,1.0,page_10",
+                "11,2.0,page_11",
+                "12,3.0,page_12",
+                "END"
+              ]
+
+#-----------------------------------------------------------------------------------------------------------------
+valuesList1a = [ (6667, 1, 1.0, 1001),
+                 (6667, 2, 2.0, 1002),
+                 (6667, 3, 3.0, 1003),
+                 (6667, 4, 1.0, 1004),
+                 (6667, 5, 2.0, 1005),
+                 (6667, 6, 3.0, 1006),
+                 (6667, 7, 1.0, 1007),
+                 (6667, 8, 2.0, 1008),
+                 (6667, 9, 3.0, 1009),
+                 (6667, 10,1.0, 1010),
+                 (6667, 11,2.0, 1011),
+                 (6667, 12,3.0, 1012),
+              ]
+
+#-----------------------------------------------------------------------------------------------------------------
+averageList1 = [ "4.5" ]
+
+#-----------------------------------------------------------------------------------------------------------------
+fullStream01 = [ "START",
+                 "VALUES",
+                 "machine_1, test_1, branch_1, changeset_1, 13, 1229477017",
+                 "1, 1.0, page_01",
+                 "2, 2.0,page_02",
+                 "3, 3.0,page_03",
+                 "4, 1.0,page_04",
+                 "5, 2.0,page_05",
+                 "6, 3.0,page_06",
+                 "7, 1.0,page_07",
+                 "8, 2.0,page_08",
+                 "9, 3.0,page_09",
+                 "10,1.0,page_10",
+                 "11,2.0,page_11",
+                 "12,3.0,page_12",
+                 "END"
+              ]
+#-----------------------------------------------------------------------------------------------------------------
+fullStream02 = [ "START",
+                 "AVERAGE",
+                 "machine_1, test_1, branch_1, changeset_1, 13, 1229477017",
+                 "2.0",
+                 "END"
+              ]
+#-----------------------------------------------------------------------------------------------------------------
+fullStream03 = [ "START",
+                 "VALUES",
+                 "machine_1, test_1, branch_1, changeset_1, 13, 1229477017",
+                 "1, 1.0",
+                 "2, 2.0",
+                 "3, 3.0,page_03",
+                 "4, 1.0,page_04",
+                 "5, 2.0,page_05",
+                 "6, 3.0",
+                 "7, 1.0,page_07",
+                 "8, 2.0,page_08",
+                 "9, 3.0",
+                 "10,1.0,page_10",
+                 "END"
+              ]
+
+#=================================================================================================================
+
+#-----------------------------------------------------------------------------------------------------------------
+def test_StringValidator():
+  py.test.raises(c.ImproperFormatException, c.StringValidator.validate, ('*'))
+  py.test.raises(c.ImproperFormatException, c.StringValidator.validate, ('hello_\t'))
+  py.test.raises(c.ImproperFormatException, c.StringValidator.validate, ('fred\nfred'))
+  py.test.raises(c.ImproperFormatException, c.StringValidator.validate, ('<sally>'))
+  py.test.raises(c.ImproperFormatException, c.StringValidator.validate, ('[wilma]'))
+  py.test.raises(c.ImproperFormatException, c.StringValidator.validate, ('this & that'))
+  assert c.StringValidator.validate(None) == ''
+  assert c.StringValidator.validate('fred') == 'fred'
+  assert c.StringValidator.validate('fred fred')  == 'fred fred'
+  assert c.StringValidator.validate('__fred')  == '__fred'
+  assert c.StringValidator.validate('fred.fred')  == 'fred.fred'
+  assert c.StringValidator.validate('0123456789')  == '0123456789'
+  assert c.StringValidator.validate('1Aa9Zz._()- ')  == '1Aa9Zz._()- '
+
+#-----------------------------------------------------------------------------------------------------------------
+def test_MetaDataFromTalos_1():
+  fakeCursor = FakeCursor(databaseSelectResponsesTest1)
+  metadata = c.MetaDataFromTalos(fakeCursor, FakeDatabaseModule, metadataTest1)
+  assert fakeCursor.inTransaction == False
+  assert metadata.machine_id == 234
+  #assert metadata.os_id == 1
+  assert metadata.test_id == 45
+  assert metadata.branch_id == 3455
+  assert metadata.build_id == 2220
+  assert metadata.test_run_id == 6667
+
+#-----------------------------------------------------------------------------------------------------------------
+def test_MetaDataFromTalos_2():
+  fakeCursor = FakeCursor(databaseSelectResponsesTest2)
+  py.test.raises(c.DatabaseException, c.MetaDataFromTalos, fakeCursor, FakeDatabaseModule, metadataTest1)
+  assert fakeCursor.inTransaction == False
+
+#-----------------------------------------------------------------------------------------------------------------
+def test_valuesReader():
+  fakeCursor = FakeCursor(databaseSelectResponsesTest1)
+  metadata = c.MetaDataFromTalos(fakeCursor, FakeDatabaseModule, metadataTest1)
+  average = c.valuesReader(fakeCursor, FakeDatabaseModule, FakeInputStream(valuesList1), metadata)
+  assert average == 2.0
+  assert fakeCursor.inTransaction == False
+
+#-----------------------------------------------------------------------------------------------------------------
+def test_averageReader():
+  fakeCursor = FakeCursor(databaseSelectResponsesTest1)
+  metadata = c.MetaDataFromTalos(fakeCursor, FakeDatabaseModule, metadataTest1)
+  average = c.averageReader(fakeCursor, FakeDatabaseModule, FakeInputStream(averageList1), metadata)
+  assert average == 4.5
+  assert fakeCursor.inTransaction == False
+
+#-----------------------------------------------------------------------------------------------------------------
+def test_handleRequestValues01():
+  fakeCursor = FakeCursor(databaseSelectResponsesTest1)
+  fakeForm = FakeForm(fullStream01)
+  s = StringIO.StringIO()
+  exitCode = c.handleRequest(fakeForm, fakeCursor, c, s)
+  value = s.getvalue()
+  assert value == "Content-type: text/plain\n\nRETURN:test_1:graph.html#runid=6667\nRETURN:test_1:2.00:graph.html#id=45&branchid=3455&machineid=234\n"
+  for testTuple, answerTuple in zip(fakeCursor.inserts["test_run_values"],valuesList1a):
+    assert testTuple  == answerTuple
+  assert fakeCursor.inTransaction == False
+  assert exitCode == None
+
+#-----------------------------------------------------------------------------------------------------------------
+def test_handleRequestValues02():
+  fakeCursor = FakeCursor(databaseSelectResponsesTest2)
+  fakeForm = FakeForm(fullStream01)
+  exitCode = c.handleRequest(fakeForm, fakeCursor, c)
+  assert fakeCursor.inTransaction == False
+  assert exitCode == 500
+
+#-----------------------------------------------------------------------------------------------------------------
+def test_handleRequestValues03():
+  fakeCursor = FakeCursor(databaseSelectResponsesTest1)
+  fakeForm = FakeForm(fullStream03)
+  exitCode = c.handleRequest(fakeForm, fakeCursor, c)
+  assert fakeCursor.inTransaction == False
+  assert exitCode == None
+
+#-----------------------------------------------------------------------------------------------------------------
+def test_handleRequestAverage01():
+  fakeCursor = FakeCursor(databaseSelectResponsesTest1)
+  fakeForm = FakeForm(fullStream02)
+  s = StringIO.StringIO()
+  exitCode = c.handleRequest(fakeForm, fakeCursor, c, s)
+  value = s.getvalue()
+  assert value == "Content-type: text/plain\n\nRETURN:test_1:2.00:graph.html#id=45&branchid=3455&machineid=234\n"
+  assert fakeCursor.inTransaction == False
+  assert exitCode == None
+
+#-----------------------------------------------------------------------------------------------------------------
+def test_handleRequestAverage02():
+  fakeCursor = FakeCursor(databaseSelectResponsesTest2)
+  fakeForm = FakeForm(fullStream02)
+  exitCode = c.handleRequest(fakeForm, fakeCursor, c)
+  assert fakeCursor.inTransaction == False
+  assert exitCode == 500
