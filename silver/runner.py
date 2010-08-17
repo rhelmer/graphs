60308cb7894d061039d394c48cdcef457e46383f
Ian Bicking <ianb@colorstudy.com>
2010-08-17 16:17:18 -0500

    Add logger, and allow translation of .cgi scripts to WSGI apps
    --HG--
    branch : 1.0

--- silver/runner.py
+++ silver/runner.py
@@ -1,9 +1,12 @@
+import sys
 import os
 from webob import exc
 from webob.dec import wsgify
 from paste.cgiapp import CGIApplication
+from paste.translogger import TransLogger
+from silversupport.env import is_production
 
-here = os.path.dirname(os.path.abspath(__file__))
+here = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 cgi_scripts = os.path.join(here, 'server')
 
 
@@ -18,11 +21,28 @@ def application(req):
         script_path = os.path.join(cgi_scripts, req.path_info.lstrip('/'))
         script_path = os.path.abspath(script_path)
         assert script_path.startswith(cgi_scripts + '/')
-        if not os.path.isfile(script_path):
+        py_name = None
+        if script_path.endswith('.cgi'):
+            py_name = script_path[:-4]
+            py_name = os.path.join(os.path.dirname(py_name), os.path.basename(py_name).replace('-', '_'))
+            py_name += '_cgi.py'
+            if not os.path.exists(py_name):
+                py_name = None
+        if (not os.path.isfile(script_path)
+            and py_name is None):
             raise exc.HTTPNotFound('Does not point to a file: %r' % script_path)
-    if script_path is None:
+    if script_path is None and py_name is None:
         raise exc.HTTPNotFound()
-    app = CGIApplication({}, script_path)
+    if py_name:
+        mod_name = os.path.basename(py_name)[:-3].replace('/', '.')
+        __import__(mod_name)
+        mod = sys.modules[mod_name]
+        app = mod.application
+        print 'Using', app
+    else:
+        app = CGIApplication({}, script_path)
+    if not is_production():
+        app = TransLogger(app)
     return app
 
 config = os.environ.get('SILVER_APP_CONFIG')
