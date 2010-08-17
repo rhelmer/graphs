77e155f24938eaa4a30975784c2b5d9086f6f62e
Ian Bicking <ianb@colorstudy.com>
2010-08-17 16:20:52 -0500

    move setup-db.cgi to a WSGI script
    --HG--
    branch : 1.0
    rename : server/setup-db.cgi => server/setup_db_cgi.py

---
+++ server/setup_db_cgi.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+from webob.dec import wsgify
+from webob import Response
+from graphsdb import db
+
+here = os.path.dirname(os.path.abspath(__file__))
+sql_file = os.path.join(here, '../sql/schema.sql')
+
+@wsgify
+def application(req):
+    cursor = db.cursor()
+    sql = open(sql_file).read()
+    sql = [
+        s.strip()
+        for s in sql.split(';')
+        if s.strip()]
+    resp = Response(content_type='text/plain')
+    for chunk in sql:
+        try:
+            cursor.execute(chunk)
+        except:
+            import sys
+            print >> resp, "Bad SQL: %s" % chunk
+            raise
+    cursor.close()
+    print >> resp, 'Setup ok'
+    return resp
