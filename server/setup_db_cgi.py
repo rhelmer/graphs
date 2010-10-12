7b5acd7ff031a7b6a2512a8e96100bee35018eb9
Ian Bicking <ianb@colorstudy.com>
2010-10-12 17:07:17 -0500

    Avoid boring setup warnings
    --HG--
    branch : 1.0

--- server/setup_db_cgi.py
+++ server/setup_db_cgi.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
 import os
+import warnings
 from webob.dec import wsgify
 from webob import Response
 from graphsdb import db
 
+# These table-exists warnings are boring:
+warnings.filterwarnings('ignore', message=r'Table.*already exists')
+
 here = os.path.dirname(os.path.abspath(__file__))
 sql_file = os.path.join(here, '../sql/schema.sql')
+
 
 @wsgify
 def application(req):
