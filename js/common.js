939cfffbc4b20563ba8b3ef321a65b487aeda9c4
Robert Helmer <robert@roberthelmer.com>
2010-12-27 15:20:00 -0800

    refactor in preparation for embed
    --HG--
    branch : 1.0

--- js/common.js
+++ js/common.js
@@ -1,5 +1,13 @@
 var displayDays = 7;
 var datatype = 'running';
+
+var _zoomFrom, _zoomTo;
+var plot, overview, ajaxSeries;
+var prevSeriesIndex = -1,
+    prevDataIndex = -1;
+
+var MAX_GRAPHS = 6;
+var MAX_CSETS = 100;
 var DAY = 86400000;
 
 var COLORS = ['#e7454c', '#6dba4b', '#4986cf', '#f5983d', '#884e9f',
@@ -28,6 +36,23 @@ var PLOT_OPTIONS = {
         autoHighlight: false
     }
 };
+
+
+    var OVERVIEW_OPTIONS = {
+        xaxis: { mode: 'time' },
+        yaxis: { min: 0 },
+        selection: { mode: 'x', color: '#97c6e5' },
+        series: {
+            lines: { show: true, lineWidth: 1 },
+            shadowSize: 0
+        },
+        grid: {
+            color: '#cdd6df',
+            borderWidth: 2,
+            backgroundColor: '#fff',
+            tickColor: 'rgba(0,0,0,0)'
+        }
+    };
 
 function debug(message)
 {
@@ -94,6 +119,39 @@ $.fn.selectBox = function() {
 
     return this;
 };
+    $.fn.showBubble = function(anchor) {
+        anchor = $(anchor);
+        var offset = anchor.offset(),
+            w = anchor.outerWidth(),
+            h = anchor.outerHeight(),
+            bubbleWrap = this.find('.bubble-wrap');
+
+        bubbleWrap.css({ left: offset.left+w/2, top: offset.top+h });
+
+        return this.bind('click.bubble', onClickBubble)
+                   .bind('copy.bubble', onCopyBubble)
+                   .show();
+
+        function onClickBubble(e) {
+            if (bubbleWrap.has(e.target).length == 0 ) {
+                $(this).hideBubble();
+                return false;
+            }
+        }
+
+        function onCopyBubble(e) {
+            if ( $(e.target).closest('input,textarea').length ) {
+                var self = $(this);
+                setTimeout(function() { self.hideBubble(); }, 100);
+            }
+        }
+    };
+
+    $.fn.hideBubble = function(anchor) {
+        return this.unbind('click.bubble')
+                   .unbind('copy.bubble')
+                   .hide();
+    };
 
 // FIXME perhaps graphserver should send us data in this format instead
 function convertData(testName, branchName, platformName, data)
@@ -193,3 +251,320 @@ function parseSeries(seriesIn, i, weight, explodedWeight)
         };
     });
 }
+function processArgs() 
+{
+    try {
+      var args = window.location.hash.split('&');
+      // FIXME order should not matter
+      if (args.length == 0) {
+          return false;
+      }
+      var tests = args[0].split('=')[1];
+      var sel;
+      if (args.length >= 2) {
+          sel = args[1].split('=')[1];
+      }
+      if (args.length >= 3) {
+          displayDays = args[2].split('=')[1];
+      }
+      if (args.length >= 4) {
+          datatype = args[3].split('=')[1];
+      }
+      if (tests) {
+          tests = JSON.parse(tests);
+          if (!confirmTooMuchData(tests.length, MAX_GRAPHS, 
+                                  'data series')) {
+              return false;
+          }
+      }
+    } catch (e) {
+        error('Could not understand URL', e);
+    }
+
+    return [tests, sel, displayDays, datatype];
+}
+function confirmTooMuchData(count, suggested, name)
+{
+    if (count > suggested) {
+        var msg = 'WARNING: You are about to load ' + count +
+              ' ' + name + '\n' +
+              'Loading more than ' + suggested +
+              ' is not recommended.\n' +
+              'Do it anyway?';
+        return window.confirm(msg);
+    }
+    return true;
+}
+function deltaPlot(plot)
+{
+    var newPlot = [];
+    var previous;
+    plot.maxV = 0;       
+    plot.minV = 0;
+    $.each(plot.data, function() {
+       var datetime = $(this)[0];
+       var elapsed = $(this)[1];
+       var newV;
+       if (previous) { 
+           if (datatype == 'delta') {
+               newV = (elapsed - previous);
+           } else if (datatype == 'deltapercent') {
+               newV = (((elapsed / previous) - 1) * 100);
+           } else {
+               error('Unknown datatype');
+               return false;
+           } 
+           newPlot.push([datetime, newV]);
+           plot.maxV = plot.maxV > newV ? plot.maxV : newV;
+           plot.minV = plot.minV < newV ? plot.minV : newV;
+       }
+       previous = elapsed;
+    });
+    plot.data = newPlot;
+    return plot;
+}
+function getZoomRange()
+{
+    return {
+        from: _zoomFrom || ajaxSeries.minT,
+        to: _zoomTo || ajaxSeries.maxT
+    };
+}
+
+function getPlotRange()
+{
+    var sel = plot.getSelection();
+    var range;
+
+    if (sel && sel.xaxis) {
+        range = sel.xaxis;
+        plot.clearSelection(true);
+    } else {
+        var oldRange = getZoomRange();
+        range = {
+            from: oldRange.from + (oldRange.to - oldRange.from) / 4,
+            to: oldRange.from + 3 * (oldRange.to - oldRange.from) / 4
+        };
+    }
+
+    return range;
+}
+
+function zoomIn()
+{
+    var range = getPlotRange();
+    zoomTo(range);
+}
+
+function zoomOut()
+{
+    var oldRange = getZoomRange();
+
+    var range = {
+        from: oldRange.from - (oldRange.to - oldRange.from) / 2,
+        to: oldRange.from + 3 * (oldRange.to - oldRange.from) / 2
+    };
+
+    var dt = 0;
+    if (range.from < ajaxSeries.minT) {
+        dt = ajaxSeries.minT - range.from;
+    } else if (range.to > ajaxSeries.maxT) {
+        dt = ajaxSeries.maxT - range.to;
+    }
+
+    range.from = Math.max(range.from + dt, ajaxSeries.minT);
+    range.to = Math.min(range.to + dt, ajaxSeries.maxT);
+
+    zoomTo(range);
+}
+
+function zoomTo(range)
+{
+    _zoomFrom = (range && range.from) || ajaxSeries.minT;
+    _zoomTo = (range && range.to) || ajaxSeries.maxT;
+
+    unlockTooltip();
+    hideTooltip(true);
+    updatePlot();
+
+    if (ajaxSeries.minT < _zoomFrom || _zoomTo < ajaxSeries.maxT) {
+        overview.setSelection({ xaxis: { from: _zoomFrom,
+                                         to: _zoomTo } }, true);
+        var canZoomOut = true;
+    } else {
+        overview.clearSelection(true);
+        var canZoomOut = false;
+    }
+
+    $('#zoomout').toggleClass('disabled', !canZoomOut);
+}
+function onPlotHover(e, pos, item)
+{
+    $('#plot').css({ cursor: item ? 'pointer' : 'crosshair' });
+
+    if (item) {
+        if (item.seriesIndex != prevSeriesIndex ||
+            item.dataIndex != prevDataIndex) {
+
+            updateTooltip(item);
+            showTooltip(item.pageX, item.pageY);
+            prevSeriesIndex = item.seriesIndex;
+            prevDataIndex = item.dataIndex;
+        }
+    } else {
+        hideTooltip();
+        prevSeriesIndex = -1;
+        prevDataIndex = -1;
+    }
+}
+
+function onPlotClick(e, pos, item)
+{
+    unlockTooltip();
+
+    if (item) {
+        updateTooltip(item);
+        showTooltip(item.pageX, item.pageY);
+        lockTooltip();
+    } else {
+        hideTooltip(true);
+    }
+}
+
+function onPlotSelect(e, ranges)
+{
+    _zoomFrom = ranges.xaxis.from;
+    _zoomTo = ranges.xaxis.to;
+}
+
+function onPlotUnSelect(e, ranges)
+{
+    _zoomFrom = null;
+    _zoomTo = null;
+}
+
+function onOverviewSelect(e, ranges)
+{
+    plot.clearSelection(true);
+    zoomTo(ranges.xaxis);
+    updateLocation();
+}
+
+function onOverviewUnselect(e)
+{
+    zoomTo(null);
+}
+
+var resizeTimer = null;
+
+function onResize()
+{
+    if (!resizeTimer) {
+        resizeTimer = setTimeout(function() {
+            updatePlot();
+            resizeTimer = null;
+        }, 50);
+    }
+}
+
+var ttHideTimer = null,
+    ttLocked = false;
+
+function updateTooltip(item)
+{
+    if (ttLocked) return;
+
+    var i = item.dataIndex,
+        s = item.series,
+        etc = s.etc;
+
+    var branch = etc.branch,
+        test = etc.test,
+        platform = etc.platform,
+        machine = etc.machine || 'mean';
+
+    var t = item.datapoint[0],
+        v = item.datapoint[1],
+        v0 = i ? s.data[i - 1][1] : v,
+        dv = v - v0,
+        dvp = v / v0 - 1,
+        changeset = etc.changesets[item.dataIndex];
+
+    $('#tt-series').html(test + ' (' + branch + ')');
+    $('#tt-series2').html(platform + ' (' + machine + ')');
+    if (datatype == 'running') {
+        $('#tt-v').html(parseInt(v) + ' ms');
+        $('#tt-dv').html('&Delta; ' + dv.toFixed(0) +
+                         ' ms (' + (100 * dvp).toFixed(1) + '%)');
+    } else if (datatype == 'delta') {
+        $('#tt-v').html('&Delta; ' + v.toFixed(3) + ' ms');
+        $('#tt-dv').html('');
+    } else if (datatype == 'deltapercent') {
+        $('#tt-v').html('&Delta; ' + v.toFixed(3) + '%');
+        $('#tt-dv').html('');
+    } else {
+        error('Unknown datatype');
+    }
+    // FIXME need a map of branches to mercurial repos...
+    var url = 'http://hg.mozilla.org/mozilla-central/rev/';
+    $('#tt-cset').html(changeset).attr('href', url + changeset);
+    $('#tt-t').html($.plot.formatDate(new Date(t), '%b %d, %y %H:%M'));
+
+    plot.unhighlight();
+    plot.highlight(s, item.datapoint);
+}
+
+function showTooltip(x, y)
+{
+    if (ttLocked) return;
+
+    var tip = $('#tooltip'),
+        w = tip.width(),
+        h = tip.height(),
+        left = x - w / 2,
+        top = y - h - 10;
+
+    if (ttHideTimer) {
+        clearTimeout(ttHideTimer);
+        ttHideTimer = null;
+    }
+
+    tip.stop(true);
+
+    if (tip.css('visibility') == 'hidden') {
+        tip.css({ opacity: 0, visibility: 'visible', left: left,
+                  top: top + 10 });
+        tip.animate({ opacity: 1, top: top }, 250);
+    } else {
+        tip.css({ opacity: 1, left: left, top: top });
+    }
+}
+
+function hideTooltip(now)
+{
+    if (ttLocked) return;
+
+    if (!ttHideTimer) {
+        ttHideTimer = setTimeout(function() {
+            ttHideTimer = null;
+            plot.unhighlight();
+            $('#tooltip').animate({ opacity: 0, top: '+=10' },
+                                    250, 'linear', function() {
+                $(this).css({ visibility: 'hidden' });
+            });
+        }, now ? 0 : 250);
+    }
+}
+
+function lockTooltip() {
+    ttLocked = true;
+    $('#tooltip').addClass('locked');
+    $('#tt-help').html('');
+}
+function unlockTooltip() {
+    ttLocked = false;
+    $('#tooltip').removeClass('locked');
+    $('#tt-help').html('Click to lock');
+}
+
+function isTooltipLocked() { return ttLocked; }
--- js/graph-2.js
+++ js/graph-2.js
@@ -1,61 +1,5 @@
 (function($) {
 
-    $.fn.showBubble = function(anchor) {
-        anchor = $(anchor);
-        var offset = anchor.offset(),
-            w = anchor.outerWidth(),
-            h = anchor.outerHeight(),
-            bubbleWrap = this.find('.bubble-wrap');
-
-        bubbleWrap.css({ left: offset.left+w/2, top: offset.top+h });
-
-        return this.bind('click.bubble', onClickBubble)
-                   .bind('copy.bubble', onCopyBubble)
-                   .show();
-
-        function onClickBubble(e) {
-            if ( bubbleWrap.has(e.target).length == 0 ) {
-                $(this).hideBubble();
-                return false;
-            }
-        }
-
-        function onCopyBubble(e) {
-            if ( $(e.target).closest('input,textarea').length ) {
-                var self = $(this);
-                setTimeout(function() { self.hideBubble(); }, 100);
-            }
-        }
-    };
-
-    $.fn.hideBubble = function(anchor) {
-        return this.unbind('click.bubble')
-                   .unbind('copy.bubble')
-                   .hide();
-    };
-
-    var OVERVIEW_OPTIONS = {
-        xaxis: { mode: 'time' },
-        yaxis: { min: 0 },
-        selection: { mode: 'x', color: '#97c6e5' },
-        series: {
-            lines: { show: true, lineWidth: 1 },
-            shadowSize: 0
-        },
-        grid: {
-            color: '#cdd6df',
-            borderWidth: 2,
-            backgroundColor: '#fff',
-            tickColor: 'rgba(0,0,0,0)'
-        }
-    };
-
-
-    var suggested_graphs = 6;
-    var suggested_csets = 100;
-
-    var plot, overview, ajaxSeries;
-    var _zoomFrom, _zoomTo;
     var minT, maxT;
     var allSeries = {};
 
@@ -68,47 +12,19 @@
     {
         initPlot();
         updateBindings();
+        var [tests, sel, displayDays, datatype] = processArgs();
+        if (tests) {
+            for (var i = 0; i < tests.length; i++) {
+                var run = tests[i];
+                var testid = run[0];
+                var branchid = run[1];
+                var platformid = run[2];
 
-        try {
-          var args = window.location.hash.split('&');
-          // FIXME order should not matter
-          if (args.length == 0) {
-              return false;
-          }
-          var tests = args[0].split('=')[1];
-          var sel;
-          if (args.length >= 2) {
-              sel = args[1].split('=')[1];
-          }
-          if (args.length >= 3) {
-              displayDays = args[2].split('=')[1];
-          }
-          if (args.length >= 4) {
-              datatype = args[3].split('=')[1];
-          }
-          if (tests) {
-              tests = JSON.parse(tests);
-              if (!confirmTooMuchData(tests.length, suggested_graphs, 
-                                      'data series')) {
-                  return false;
-              }
-                
-              for (var i = 0; i < tests.length; i++)
-              {
-                  var run = tests[i];
-                  var testid = run[0];
-                  var branchid = run[1];
-                  var platformid = run[2];
-
-                  fetchData(testid, branchid, platformid, sel);
-              }
-          } else {
-              addMoreTestData();
-          }
-        } catch (e) {
-            error('Could not understand URL', e);
+                fetchData(testid, branchid, platformid, sel);
+            }
+        } else {
+            addMoreTestData();
         }
-
         $('.selectBox').selectBox();
     }
 
@@ -232,111 +148,6 @@
         hideTooltip(true);
         plot = $.plot($('#plot'), plotData, plotOptions);
         overview = $.plot($('#overview'), overviewData, overviewOptions);
-    }
-
-    function deltaPlot(plot)
-    {
-        var newPlot = [];
-        var previous;
-        plot.maxV = 0;       
-        plot.minV = 0;
-        $.each(plot.data, function() {
-           var datetime = $(this)[0];
-           var elapsed = $(this)[1];
-           var newV;
-           if (previous) { 
-               if (datatype == 'delta') {
-                   newV = (elapsed - previous);
-               } else if (datatype == 'deltapercent') {
-                   newV = (((elapsed / previous) - 1) * 100);
-               } else {
-                   error("Unknown datatype");
-                   return false;
-               } 
-               newPlot.push([datetime, newV]);
-               plot.maxV = plot.maxV > newV ? plot.maxV : newV;
-               plot.minV = plot.minV < newV ? plot.minV : newV;
-           }
-           previous = elapsed;
-        });
-        plot.data = newPlot;
-        return plot;
-    }
-
-    function getZoomRange()
-    {
-        return {
-            from: _zoomFrom || ajaxSeries.minT,
-            to: _zoomTo || ajaxSeries.maxT
-        };
-    }
-
-    function getPlotRange()
-    {
-        var sel = plot.getSelection();
-        var range;
-
-        if (sel && sel.xaxis) {
-            range = sel.xaxis;
-            plot.clearSelection(true);
-        } else {
-            var oldRange = getZoomRange();
-            range = {
-                from: oldRange.from + (oldRange.to - oldRange.from) / 4,
-                to: oldRange.from + 3 * (oldRange.to - oldRange.from) / 4
-            };
-        }
-
-        return range;
-    }
-
-    function zoomIn()
-    {
-        var range = getPlotRange();
-        zoomTo(range);
-    }
-
-    function zoomOut()
-    {
-        var oldRange = getZoomRange();
-
-        var range = {
-            from: oldRange.from - (oldRange.to - oldRange.from) / 2,
-            to: oldRange.from + 3 * (oldRange.to - oldRange.from) / 2
-        };
-
-        var dt = 0;
-        if (range.from < ajaxSeries.minT) {
-            dt = ajaxSeries.minT - range.from;
-        } else if (range.to > ajaxSeries.maxT) {
-            dt = ajaxSeries.maxT - range.to;
-        }
-
-        range.from = Math.max(range.from + dt, ajaxSeries.minT);
-        range.to = Math.min(range.to + dt, ajaxSeries.maxT);
-
-        zoomTo(range);
-    }
-
-    function zoomTo(range)
-    {
-        _zoomFrom = (range && range.from) || ajaxSeries.minT;
-        _zoomTo = (range && range.to) || ajaxSeries.maxT;
-
-        unlockTooltip();
-        hideTooltip(true);
-        updatePlot();
-
-        if (ajaxSeries.minT < _zoomFrom || _zoomTo < ajaxSeries.maxT) {
-            overview.setSelection({ xaxis: { from: _zoomFrom,
-                                             to: _zoomTo } }, true);
-            var canZoomOut = true;
-        } else {
-            overview.clearSelection(true);
-            var canZoomOut = false;
-        }
-
-        $('#zoomout').toggleClass('disabled', !canZoomOut);
     }
 
     function onPageKeyDown(e)
@@ -552,188 +363,6 @@
         e.preventDefault();
     }
 
-    var prevSeriesIndex = -1,
-        prevDataIndex = -1;
-
-    function onPlotHover(e, pos, item)
-    {
-        $('#plot').css({ cursor: item ? 'pointer' : 'crosshair' });
-
-        if (item) {
-            if (item.seriesIndex != prevSeriesIndex ||
-                item.dataIndex != prevDataIndex) {
-
-                updateTooltip(item);
-                showTooltip(item.pageX, item.pageY);
-                prevSeriesIndex = item.seriesIndex;
-                prevDataIndex = item.dataIndex;
-            }
-        } else {
-            hideTooltip();
-            prevSeriesIndex = -1;
-            prevDataIndex = -1;
-        }
-    }
-
-    function onPlotClick(e, pos, item)
-    {
-        unlockTooltip();
-
-        if (item) {
-            updateTooltip(item);
-            showTooltip(item.pageX, item.pageY);
-            lockTooltip();
-        } else {
-            hideTooltip(true);
-        }
-    }
-
-    function onPlotSelect(e, ranges)
-    {
-        _zoomFrom = ranges.xaxis.from;
-        _zoomTo = ranges.xaxis.to;
-    }
-
-    function onPlotUnSelect(e, ranges)
-    {
-        _zoomFrom = null;
-        _zoomTo = null;
-    }
-
-    function onOverviewSelect(e, ranges)
-    {
-        plot.clearSelection(true);
-        zoomTo(ranges.xaxis);
-        updateLocation();
-    }
-
-    function onOverviewUnselect(e)
-    {
-        zoomTo(null);
-    }
-
-    var resizeTimer = null;
-
-    function onResize()
-    {
-        if (!resizeTimer) {
-            resizeTimer = setTimeout(function() {
-                updatePlot();
-                resizeTimer = null;
-            }, 50);
-        }
-    }
-
-    var ttHideTimer = null,
-        ttLocked = false;
-
-    function updateTooltip(item)
-    {
-        if (ttLocked) return;
-
-        var i = item.dataIndex,
-            s = item.series,
-            etc = s.etc;
-
-        var branch = etc.branch,
-            test = etc.test,
-            platform = etc.platform,
-            machine = etc.machine || 'mean';
-
-        var t = item.datapoint[0],
-            v = item.datapoint[1],
-            v0 = i ? s.data[i - 1][1] : v,
-            dv = v - v0,
-            dvp = v / v0 - 1,
-            changeset = etc.changesets[item.dataIndex];
-
-        $('#tt-series').html(test + ' (' + branch + ')');
-        $('#tt-series2').html(platform + ' (' + machine + ')');
-        if (datatype == 'running') {
-            $('#tt-v').html(parseInt(v) + ' ms');
-            $('#tt-dv').html('&Delta; ' + dv.toFixed(0) +
-                             ' ms (' + (100 * dvp).toFixed(1) + '%)');
-        } else if (datatype == 'delta') {
-            $('#tt-v').html('&Delta; ' + v.toFixed(3) + ' ms');
-            $('#tt-dv').html('');
-        } else if (datatype == 'deltapercent') {
-            $('#tt-v').html('&Delta; ' + v.toFixed(3) + '%');
-            $('#tt-dv').html('');
-        } else {
-            error('Unknown datatype');
-        }
-        // FIXME need a map of branches to mercurial repos...
-        var url = 'http://hg.mozilla.org/mozilla-central/rev/';
-        $('#tt-cset').html(changeset).attr('href', url + changeset);
-        $('#tt-t').html($.plot.formatDate(new Date(t), '%b %d, %y %H:%M'));
-
-        plot.unhighlight();
-        plot.highlight(s, item.datapoint);
-    }
-
-    function showTooltip(x, y)
-    {
-        if (ttLocked) return;
-
-        var tip = $('#tooltip'),
-            w = tip.width(),
-            h = tip.height(),
-            left = x - w / 2,
-            top = y - h - 10;
-
-        if (ttHideTimer) {
-            clearTimeout(ttHideTimer);
-            ttHideTimer = null;
-        }
-
-        tip.stop(true);
-
-        if (tip.css('visibility') == 'hidden') {
-            tip.css({ opacity: 0, visibility: 'visible', left: left,
-                      top: top + 10 });
-            tip.animate({ opacity: 1, top: top }, 250);
-        } else {
-            tip.css({ opacity: 1, left: left, top: top });
-        }
-    }
-
-    function hideTooltip(now)
-    {
-        if (ttLocked) return;
-
-        if (!ttHideTimer) {
-            ttHideTimer = setTimeout(function() {
-                ttHideTimer = null;
-                plot.unhighlight();
-                $('#tooltip').animate({ opacity: 0, top: '+=10' },
-                                        250, 'linear', function() {
-                    $(this).css({ visibility: 'hidden' });
-                });
-            }, now ? 0 : 250);
-        }
-    }
-
-    function lockTooltip() {
-        ttLocked = true;
-        $('#tooltip').addClass('locked');
-        $('#tt-help').html('');
-    }
-    function unlockTooltip() {
-        ttLocked = false;
-        $('#tooltip').removeClass('locked');
-        $('#tt-help').html('Click to lock');
-    }
-
-    function isTooltipLocked() { return ttLocked; }
-
-    function debug(message)
-    {
-      if (typeof(console) !== 'undefined' && console != null) {
-        console.log(message);
-        console.log(JSON.stringify(message));
-      }
-    }
-
     function fetchData(testid, branchid, platformid, sel) {
         var uniqueSeries = 'series_' + testid + '_' + branchid + '_' +
                            platformid;
@@ -834,7 +463,7 @@
             });
         });
 
-        if (!confirmTooMuchData(csets.length, suggested_csets, 'changesets')) {
+        if (!confirmTooMuchData(csets.length, MAX_CSETS, 'changesets')) {
             return false;
         }
         window.open('http://hg.mozilla.org/mozilla-central/pushloghtml?changeset=' + csets.join('&changeset='));
@@ -883,7 +512,7 @@
                 count += 1;
             }
         });
-        if (!confirmTooMuchData(count, suggested_graphs, 'data series')) {
+        if (!confirmTooMuchData(count, MAX_CSETS, 'data series')) {
             addMoreTestData();
             return false;
         }
@@ -1001,18 +630,6 @@
         window.location = newLocation;
     }
 
-    function confirmTooMuchData(count, suggested, name)
-    {
-        if (count > suggested) {
-            var msg = 'WARNING: You are about to load ' + count +
-                  ' ' + name + '\n' +
-                  'Loading more than ' + suggested +
-                  ' is not recommended.\n' +
-                  'Do it anyway?';
-            return window.confirm(msg);
-        }
-        return true;
-    }
 
     $('#chart-link').click(function() {
         $('#link-overlay').showBubble(this);
