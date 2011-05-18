696c14f5f1c6243da28c5a825cfd57a3ca564428
Robert Helmer <rhelmer@rhelmer.org>
2011-05-18 11:29:34 -0700

    pass servername as arg to cache script

--- scripts/cache.sh
+++ scripts/cache.sh
@@ -4,10 +4,16 @@
 # TODO static_graphs.js should be able to replace this.
 #
 
+SERVERNAME=$1
+if [ -z "$SERVERNAME" ]
+then
+  echo "Syntax: cache.sh <servername>"
+fi
+
 runs="id=16&branchid=1&platformid=12 id=16&branchid=1&platformid=1 id=16&branchid=1&platformid=13 id=16&branchid=1&platformid=14 id=38&branchid=1&platformid=12 id=38&branchid=1&platformid=1 id=38&branchid=1&platformid=13 id=38&branchid=1&platformid=14 id=25&branchid=1&platformid=12 id=25&branchid=1&platformid=1 id=25&branchid=1&platformid=13 id=25&branchid=1&platformid=14"
 for run in $runs
 do
-  curl -H 'Accept-encoding: gzip' -s "http://bm-graphs-stage01.mozilla.org/api/test/runs?${run}" > /dev/null
+  curl -o /dev/null -s -H 'Accept-encoding: gzip' -s "http://${SERVERNAME}/api/test/runs?${run}"
 done
 
-curl -H 'Accept-encoding: gzip' -s "http://bm-graphs-stage01.mozilla.org/api/test?attribute=short" > /dev/null
+curl -o /dev/null -s -H 'Accept-encoding: gzip' -s "http://${SERVERNAME}/api/test?attribute=short"
