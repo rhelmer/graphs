rhelmer/graphs

commit d1c39460e84a8b80631c0423ba5f5992187bb487

bug 705293 - adjust Y axis according to timeline. stop forcing yaxis min to 0, just let flot handle it - in fact the whole approach here is to simply not plot the datapoints that aren't being displayed, which allows flot to do what we expect.

js/common.js
@@ -164,8 +164,6 @@ GraphCommon.convertData = function(testName, branchName, platformName, data,
         'branch': branchName,
         'maxT': undefined,
         'minT': undefined,
-        'maxV': undefined,
-        'minV': undefined,
         'platform': platformName,
         'runs': [],
         'test': testName,
@@ -183,8 +181,6 @@ GraphCommon.convertData = function(testName, branchName, platformName, data,
                                     this.displayDaysInMicroseconds();
     gdata.minT = new Date().getTime() - displayDaysInMicroseconds;
     gdata.maxT = new Date().getTime();
-    gdata.minV = data['min'];
-    gdata.maxV = data['max'];
 
     minT = gdata.minT;
     maxT = gdata.maxT;
@@ -257,11 +253,19 @@ GraphCommon.parseSeries = function(seriesIn, i, weight, explodedWeight)
     var plots = [];
     $.each(datasets, function(index) {
         d = datasets[index];
+        var displayDays = GraphCommon.displayDaysInMicroseconds();
+        var minVisibleT = new Date().getTime() - displayDays;
+        var maxVisibleT = new Date().getTime();
+
         var plot = {
             id: d.id,
             lines: { lineWidth: lineWidth },
             color: color,
-            data: $.map(d.data, function(p) { return [[p.t, p.v]]; }),
+            data: $.map(d.data, function(p) { 
+                if (p.t >= minVisibleT && p.t <= maxVisibleT) {
+                    return [[p.t, p.v]];
+                }
+            }),
             etc: {
                 branch: seriesIn.branch,
                 test: seriesIn.test,
@@ -312,8 +316,6 @@ GraphCommon.deltaPlot = function(plot)
 {
     var newPlot = [];
     var previous;
-    plot.maxV = 0;
-    plot.minV = 0;
     $.each(plot.data, function() {
        var datetime = $(this)[0];
        var elapsed = $(this)[1];
@@ -328,8 +330,6 @@ GraphCommon.deltaPlot = function(plot)
                return false;
            }
            newPlot.push([datetime, newV]);
-           plot.maxV = plot.maxV > newV ? plot.maxV : newV;
-           plot.minV = plot.minV < newV ? plot.minV : newV;
        }
        previous = elapsed;
     });
@@ -617,10 +617,9 @@ GraphCommon.updatePlot = function()
     var plotData = [];
     var overviewData = [];
     var plotOptions, overviewOptions;
-    var maxV = 0,
-        marginV = 0,
-        minV = 0;
     var count = 0;
+    var displayDays = GraphCommon.displayDaysInMicroseconds();
+
     $.each(this.allSeries, function(index, series) {
         if ($.isEmptyObject(series)) {
             // purposely deleted, keep colors consistent
@@ -637,11 +636,6 @@ GraphCommon.updatePlot = function()
             }
             if (GraphCommon.datatype != 'running') {
                 plot = GraphCommon.deltaPlot(plot);
-                maxV = plot.maxV > maxV ? plot.maxV : maxV;
-                minV = plot.minV < minV ? plot.minV : minV;
-            } else {
-                maxV = maxV > series.maxV ? maxV : series.maxV;
-                minV = minV < series.minV ? minV : series.minV;
             }
             plotData.push(plot);
         }
@@ -657,14 +651,12 @@ GraphCommon.updatePlot = function()
 
         count++;
 
-        marginV = 0.1 * (maxV - minV);
         minT = GraphCommon.zoomFrom ||
                (minT < series.minT ? minT : series.minT);
         maxT = GraphCommon.zoomTo || (maxT > series.maxT ? maxT : series.maxT);
 
-        var displayDays = GraphCommon.displayDaysInMicroseconds();
         var xaxis = { xaxis: { min: minT, max: maxT } },
-            yaxis = { yaxis: { min: minV, max: maxV + marginV } };
+            yaxis = { yaxis: {} };
         var overview_xaxis = { xaxis: { min: new Date() - displayDays,
                                         max: new Date() } };
         plotOptions = $.extend(true, { }, PLOT_OPTIONS, xaxis, yaxis),

js/config.js
@@ -24,7 +24,6 @@ if ($.color) {
 
 var PLOT_OPTIONS = {
     xaxis: { mode: 'time' },
-    yaxis: { min: 0 },
     selection: { mode: 'x', color: '#97c6e5' },
     /* crosshair: { mode: 'xy', color: '#cdd6df', lineWidth: 1 }, */
     series: { shadowSize: 0 },
@@ -42,7 +41,6 @@ var PLOT_OPTIONS = {
 
 var OVERVIEW_OPTIONS = {
     xaxis: { mode: 'time' },
-    yaxis: { min: 0 },
     selection: { mode: 'x', color: '#97c6e5' },
     series: {
         lines: { show: true, lineWidth: 1 },

commit 297d12a075d66ba60cb0000d515996672d85a645

Fix regressions.
1. My y-axis zooming change (c1fc48d) caused tickFormatter to not round properly.
Round at 4th decimal place so that values under that will be ignored.

2. Also fix a regression from d1c3946 for "bug 705293 - adjust Y axis according
to timeline". It didn't trim entries in d.data for changesets and additional
changesets, and ended up confusing the graph.

js/common.js
@@ -262,22 +262,24 @@ GraphCommon.parseSeries = function(seriesIn, i, weight, explodedWeight)
         var minVisibleT = new Date().getTime() - displayDays;
         var maxVisibleT = new Date().getTime();
 
+        var visibleData = d.data.filter(function(p) {
+            return p.t >= minVisibleT && p.t <= maxVisibleT;
+        });
+
         var plot = {
             id: d.id,
             lines: { lineWidth: lineWidth },
             color: color,
-            data: $.map(d.data, function(p) {
-                if (p.t >= minVisibleT && p.t <= maxVisibleT) {
-                    return [[p.t, p.v]];
-                }
-            }),
+            data: $.map(visibleData,
+                        function (p) { return [[p.t, p.v]]; }),
             etc: {
                 branch: seriesIn.branch,
                 test: seriesIn.test,
                 platform: seriesIn.platform,
                 machine: d.machine,
-                changesets: $.map(d.data, function(p) {return p.changeset;}),
-                additionalChangesets: $.map(d.data, function(p) {
+                changesets: $.map(visibleData,
+                                  function(p) { return p.changeset; }),
+                additionalChangesets: $.map(visibleData, function(p) {
                     return p['additionalChangesets']; })
             }
         };
@@ -675,7 +677,10 @@ GraphCommon.updatePlot = function()
         if (yaxisIndex <= 0 && series.visible) {
             if (unit) {
                 yaxes.push({tickFormatter: (function(unit) {
-                    return function(value, axis) { return value + ' ' + unit; }
+                    // FIXME This won't work for values less than 0.0001.
+                    return function(value, axis) {
+                        return Math.round(value * 10000) / 10000 + ' ' + unit;
+                    }
                 })(unit)});
             } else {
                 yaxes.push({});